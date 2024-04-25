49c4826d433289e606001b1505fa4680d4f605ce
agpavlik <agpavlik@gmail.com>
2024-04-25 18:32:08 -0400

    Styling MealItem.js

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -17,16 +17,16 @@ function MealsOverviewScreen({ route }) {
   });
 
   function renderMealItem(itemData) {
-    // const item = itemData.item;
+    const item = itemData.item;
 
-    // const mealItemProps = {
-    //   title: item.title,
-    //   imageUrl: item.imageUrl,
-    //   affordability: item.affordability,
-    //   complexity: item.complexity,
-    //   duration: item.duration,
-    // };
-    return <MealItem title={itemData.item.title} />;
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      affordability: item.affordability,
+      complexity: item.complexity,
+      duration: item.duration,
+    };
+    return <MealItem {...mealItemProps} />;
   }
 
   return (
