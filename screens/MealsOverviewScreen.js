f1e019d743f64c383f2327411046631956e296ac
agpavlik <agpavlik@gmail.com>
2024-04-25 00:27:21 -0400

    Display meals

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,13 +1,41 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, FlatList } from "react-native";
 
+import MealItem from "../components/MealItem";
 import { MEALS } from "../data/data";
 
 function MealsOverviewScreen({ route }) {
-  //  Get the ID off the category that is passed through navigation to this screen
+  // Get the ID off the category that is passed through navigation to this screen
   const catId = route.params.categoryId;
+
+  // Load all the meals belong to exact category.
+  // Return true if a meal has this categoryId in its array of category Ids.
+  // Use indexOff for that and then we can use indexOf to get the indexOf item.
+  // If we have a index greater or equal than zero, a category is part of that meal
+  // because if we would not find a given category Id in that array indexOf would return minus one.
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    // const item = itemData.item;
+
+    // const mealItemProps = {
+    //   title: item.title,
+    //   imageUrl: item.imageUrl,
+    //   affordability: item.affordability,
+    //   complexity: item.complexity,
+    //   duration: item.duration,
+    // };
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
