5e4dddeef0b7f733b6466cc429bf7386f322b98b
agpavlik <agpavlik@gmail.com>
2024-06-16 22:32:48 -0400

    Set the app version in app.json

---
+++ README.md
@@ -0,0 +1,8 @@
+React Native
+context API
+Redux Toolkit
+React Navigation package (Drawer Navigator, Tabs Navigator )
+
+User can browse around meals and dive into the details of every meal
+User can favorite or unfavorite different meals.
+User can view all the favorites on the favorites screen.
--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,27 +1,34 @@
 import { Text, View, StyleSheet } from "react-native";
 
-import { useContext } from "react";
+// import { useContext } from "react";
 
 import MealsList from "../components/MealsList/MealsList";
-import { FavoritesContext } from "../store/context/favorites-context";
+// import { FavoritesContext } from "../store/context/favorites-context";
 import { MEALS } from "../data/data";
+import { useSelector } from "react-redux";
 
 function FavoritesScreen() {
   //useContext returns the context value for the context.
   // To determine the context value, React searches the component tree
   // and finds the closest context provider above for that particular context.
-  const favoriteMealsCtx = useContext(FavoritesContext);
+  // const favoriteMealsCtx = useContext(FavoritesContext);
+
+  const favoriteMealIds = useSelector((state) => state.favoriteMeals.ids);
 
   // Filter will return an array with all the meals
   // that have an entry in IDs array in context.
+  // const favoriteMeals = MEALS.filter((meal) =>
+  //   favoriteMealsCtx.ids.includes(meal.id)
+  // );
+
   const favoriteMeals = MEALS.filter((meal) =>
-    favoriteMealsCtx.ids.includes(meal.id)
+    favoriteMealIds.includes(meal.id)
   );
 
   if (favoriteMeals.length === 0) {
     return (
       <View style={styles.rootContainer}>
-        <Text>No favorite meales added yet</Text>
+        <Text style={styles.text}>No favorite meales added yet</Text>
       </View>
     );
   }
